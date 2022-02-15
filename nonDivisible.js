aed383c273795bdf6419a8fe4ed806188ce05272
shellieD <shellie.downie13@gmail.com>
2022-02-15 20:14:24 +0000

    Add for loop - each value of S % K

--- nonDivisible.js
+++ nonDivisible.js
@@ -1,8 +1,12 @@
 
 
 function nonDivisibleSubset(s, k) {
-    remainderCount = new Array(k).fill(0);
+    remainderCount = new Array(k).fill(0); // create an empty array to count the value of the num of remainders
     console.log(remainderCount);
+    for (let i = 0; i < s.length; i++){
+        remainder = i % k;
+        remainderCount[remainder] += 1;
+        console.log(remainderCount);
+    }
 }
-
 nonDivisibleSubset([1, 2, 3, 4], 4);
