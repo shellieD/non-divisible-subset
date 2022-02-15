777d9f73a550cba24464f7c3cdfb68aff268d72f
shellieD <shellie.downie13@gmail.com>
2022-02-15 21:13:48 +0000

    FIX: bug in for loop not tallyig remainders  correctly.

--- nonDivisible.js
+++ nonDivisible.js
@@ -1,12 +1,22 @@
 
 
 function nonDivisibleSubset(s, k) {
-    remainderCount = new Array(k).fill(0); // create an empty array to count the value of the num of remainders
+    let remainderCount = new Array(k).fill(0); // create an empty array to count the value of the num of remainders
     console.log(remainderCount);
-    for (let i = 0; i < s.length; i++){
-        remainder = i % k;
-        remainderCount[remainder] += 1;
+    for (i in s){ //loop finds remainder after each value in S is modulo by k - adds 1 to remainderCount for each found
+        remainder = s[i] % k;
+        console.log(remainder);
+        remainderCount[remainder] +=1;
         console.log(remainderCount);
     }
+
+    // only one number that is excactly divisible by K can be added to the subset
+    // at index 0 the count shows how many numbers in the array are exactly divisible by 1
+    // if there are no numbers exactly divisible, take none, if there are more than one only take one
+    // and add it to the subset total.
+    subset = (Math.min(remainderCount[0], 1));
+    console.log(subset);
 }
-nonDivisibleSubset([1, 2, 3, 4], 4);
+
+
+nonDivisibleSubset([19, 10, 12, 10, 24, 25, 22], 4);
